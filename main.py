8fd34ad1530829290d1b964743a24e0ada5f46d3
grishian <grishian@hotmail.com>
2023-11-27 17:49:11 +0100

    shellcontext toegevoegd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from app import create_app, db
+from app.bp_general.models import User
 #vergeet hier de blueprints voor de models niet
 
 
@@ -7,6 +8,9 @@ sys.dont_write_bytecode = True
 app = create_app()
 
 #vergeet hier de shellcontext niet
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
 
 
 if __name__ == '__main__':
