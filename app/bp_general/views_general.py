63ddac69244bd52c01edb342703abfec9b97a728
grishian <grishian@hotmail.com>
2023-11-28 15:50:26 +0100

    checken of de database werkt op googlecloud

--- app/bp_general/views_general.py
+++ app/bp_general/views_general.py
@@ -5,7 +5,9 @@ from app.bp_general.models import User
 @bp_general.route('/')
 @bp_general.route('/index')
 def do_home():
-    return render_template('general/home.html', title='Home')
+    users = User.query.all()
+
+    return render_template('general/home.html', title='Home', users=users)
 
 
 
