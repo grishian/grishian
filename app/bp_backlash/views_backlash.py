121fef385e21c7aeb223d0f848b344e31a5f1b42
grishian <grishian@hotmail.com>
2023-12-02 10:55:55 +0100

    backlash waardes op een pagina geprint en een slappe erropage gemaakt

--- app/bp_backlash/views_backlash.py
+++ app/bp_backlash/views_backlash.py
@@ -9,80 +9,91 @@ def do_backlash():
 
     if request.method == "POST":
 
-        pinion_1 = request.form.get('input_voorkant')
-        backlash_1 = request.form.get('input_achterkant1')
-        backlash_2 = request.form.get('input_achterkant2')
-        backlash_3 = request.form.get('input_achterkant3')
-        backlash_4 = request.form.get('input_achterkant4')
+        try:
+            pinion_1 = request.form.get('input_voorkant')
+            backlash_1 = request.form.get('input_achterkant1')
+            backlash_2 = request.form.get('input_achterkant2')
+            backlash_3 = request.form.get('input_achterkant3')
+            backlash_4 = request.form.get('input_achterkant4')
 
-        pinion_abs_1 = abs(4.5-float(pinion_1))
-        pinion_abs_2 = abs(2.6-float(pinion_1))
+                
+            pinion_abs_1 = abs(4.5-float(pinion_1))
+            pinion_abs_2 = abs(2.6-float(pinion_1))
 
-        if pinion_abs_1 <= pinion_abs_2:
-            pinion = 4.5-float(pinion_1)
-        else:
-            pinion = 2.6-float(pinion_1)
+            if pinion_abs_1 <= pinion_abs_2:
+                pinion = 4.5-float(pinion_1)
+            else:
+                pinion = 2.6-float(pinion_1)
 
 
-        backlash_values = [backlash_1, backlash_2, backlash_3, backlash_4]
+            backlash_values = [backlash_1, backlash_2, backlash_3, backlash_4]
 
-        backlash_good_values = [25, 26, 27, 28, 29, 30, 31, 32, 33]
-        backlash_medium_values = [20, 21, 22, 23, 24, 34, 35]
-        good_values = []
-        medium_values = []
-        bad_values = []
+            backlash_good_values = [25, 26, 27, 28, 29, 30, 31, 32, 33]
+            backlash_medium_values = [20, 21, 22, 23, 24, 34, 35]
+            good_values = []
+            medium_values = []
+            bad_values = []
 
-        for backlash_value in backlash_values:
-            if 35 < int(backlash_value) or int(backlash_value) < 20:
-                try:
-                    bad_values.append(backlash_value)
+            for backlash_value in backlash_values:
+                if 35 < int(backlash_value) or int(backlash_value) < 20:
+                    try:
+                        bad_values.append(backlash_value)
+                            
+
+                    except Exception as e:  # pragma: no cover
+                        # if this fails we do not care, but we certainly do not want to block
+                        # someone logging in
+                        return render_template('error_pages.default_error_page.html')
+
+
+            for backlash_value in backlash_values:
+                for backlash_good_value in backlash_good_values:
+                    try:
+                        print(int(backlash_value) , int(backlash_good_value))
+                        if int(backlash_value) == int(backlash_good_value):
+                            print('is good')
+                            good_values.append(backlash_value)
+                            
+
+                    except Exception as e:  # pragma: no cover
+                        # if this fails we do not care, but we certainly do not want to block
+                        # someone logging in
+                        print('Error: {}'.format(e))
+
+            for backlash_value in backlash_values:
+                for backlash_medium_value in backlash_medium_values:
+                    try:
+
+                        if int(backlash_value) == int(backlash_medium_value):
+                            print('is medium')
+                            medium_values.append(backlash_value)
+
+
+                    except Exception as e:  # pragma: no cover
+                        # if this fails we do not care, but we certainly do not want to block
+                        # someone logging in
+                        print('Error: {}'.format(e))
+
+            
+                        
+            '''print('good values:', good_values)
+            print('medium values:', medium_values)
+            print('bad values:', bad_values)
+            print('backlashvalues', backlash_values)
+            print('meting: ', pinion_1, 'load: ', pinion)'''
+
+            return render_template('backlash/backlash_solution.html', meting=pinion_1, load=round(pinion),
+                                backlash_values=backlash_values, good_values=good_values, medium_values=medium_values, bad_values=bad_values)
                         
 
-                except Exception as e:  # pragma: no cover
-                    # if this fails we do not care, but we certainly do not want to block
-                    # someone logging in
-                    print('Error: {}'.format(e))
+        except Exception as e:  # pragma: no cover
+            # if this fails we do not care, but we certainly do not want to block
+            # someone logging in
+            return render_template('error_pages/default_error_page.html')
 
 
-        for backlash_value in backlash_values:
-            for backlash_good_value in backlash_good_values:
-                try:
-                    print(int(backlash_value) , int(backlash_good_value))
-                    if int(backlash_value) == int(backlash_good_value):
-                        print('is good')
-                        good_values.append(backlash_value)
-                        
-
-                except Exception as e:  # pragma: no cover
-                    # if this fails we do not care, but we certainly do not want to block
-                    # someone logging in
-                    print('Error: {}'.format(e))
-
-        for backlash_value in backlash_values:
-            for backlash_medium_value in backlash_medium_values:
-                try:
-
-                    if int(backlash_value) == int(backlash_medium_value):
-                        print('is medium')
-                        medium_values.append(backlash_value)
 
 
-                except Exception as e:  # pragma: no cover
-                    # if this fails we do not care, but we certainly do not want to block
-                    # someone logging in
-                    print('Error: {}'.format(e))
-
-        
-                    
-        print('good values:', good_values)
-        print('medium values:', medium_values)
-        print('bad values:', bad_values)
-
-        print('backlashvalues', backlash_values)
-
-        print('meting: ', pinion_1, 'load: ', pinion)
-
-        return render_template('backlash/backlash_solution.html', meting=pinion_1, load=round(pinion))
         
 
     return render_template('backlash/backlash.html', title='Backlash')
